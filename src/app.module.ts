48015bfd0b9b09d641e318fbe00f4c84e1218b57
Pavel <sh1k@bk.ru>
2021-07-17 14:49:07 +0300

    make it better

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import {Module} from "@nestjs/common"
+import {TrackModule} from "./track/track.module"
+import {MongooseModule} from "@nestjs/mongoose"
+import {FileModule} from "./file/file.module"
+import {ServeStaticModule} from "@nestjs/serve-static"
+import * as path from "path"
+import {AuthModule} from "./auth/auth.module"
+import {UsersModule} from "./users/users.module"
+import {RolesModule} from "./roles/roles.module"
+import {JwtRefreshTokenStrategy} from "./auth/jwt-refresh-token.strategy"
+import {PassportModule} from "@nestjs/passport"
+import {PostModule} from './post/post.module'
+
+
+@Module({
+   imports: [
+      ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+      MongooseModule.forRoot((
+        process.env.MONGODB_STORE_URI ||
+        'mongodb+srv://admin:eE6gvSCDFnXhyv7S@cluster0.enp62.mongodb.net/music-platform?retryWrites=true&w=majority')),
+      TrackModule,
+      PostModule,
+      FileModule,
+      AuthModule,
+      UsersModule,
+      RolesModule,
+      PassportModule
+   ],
+   providers: [JwtRefreshTokenStrategy]
+})
+export class AppModule {
+}
---
+++ src/post/post.controller.ts
@@ -0,0 +1,59 @@
+import {Body, Delete, Get, Param, Post, Query, Req, UploadedFiles, UseGuards, UseInterceptors} from "@nestjs/common"
+import {Controller} from "@nestjs/common"
+import {PostService} from "./post.service"
+import {ObjectId} from "mongoose"
+import {Roles} from '../auth/roles-auth.decorator'
+import {RolesGuard} from '../auth/roles.guard'
+import {JwtAuthGuard} from '../auth/jwt-auth.guard'
+import {Request} from 'express'
+import RequestWithUser from '../auth/requestWithUser.interface'
+import {CreatePostDto} from './dto/create-post.dto'
+
+
+@Controller('/posts')
+export class PostController {
+   constructor(private postService: PostService) {}
+
+   @UseGuards(JwtAuthGuard)
+   @Post()
+   create(@Body() dto: CreatePostDto,
+          @Req() req: RequestWithUser) {
+      return this.postService.create(dto, req.user)
+   }
+
+   @UseGuards(JwtAuthGuard)
+   @Get()
+   getAll(@Query('count') count: number,
+          @Query('offset') offset: number) {
+      return this.postService.getAll(count, offset)
+   }
+
+   @UseGuards(JwtAuthGuard)
+   @Get('/me')
+   getMyPosts(@Req() req: Request) {
+      return this.postService.getMyPosts(req.user)
+   }
+
+   @Roles('OWNER')
+   @UseGuards(RolesGuard)
+   @Post('/category')
+   createCategory(@Body() name: string) {
+      return this.postService.createCategory(name)
+   }
+
+   @Roles('OWNER' || 'ADMIN')
+   @UseGuards(RolesGuard)
+   @Delete(':id')
+   delete(@Param('id') id: ObjectId) {
+      return this.postService.deletePost(id)
+   }
+
+   @Roles('OWNER' || 'ADMIN')
+   @UseGuards(RolesGuard)
+   @Get('/category/:id')
+   getPostsCategory(@Param('id') id: ObjectId) {
+      return this.postService.getPostsCategory(id)
+   }
+
+
+}
