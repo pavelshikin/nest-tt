c3ebae449ea9795befa93ab36f998d59b1ceb491
Pavel <sh1k@bk.ru>
2021-07-19 21:10:39 +0300

    fix Cors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import * as cookieParser from 'cookie-parser';
+import {NestFactory} from '@nestjs/core'
+import {AppModule} from './app.module'
+import * as cookieParser from 'cookie-parser'
 import {ValidationPipe} from './pipes/validation.pipe'
 
 
 const start = async () => {
-    try {
-        const PORT = process.env.PORT || 5000
-        const app = await NestFactory.create(AppModule)
+   try {
+      const PORT = process.env.PORT || 5000
+      const app = await NestFactory.create(AppModule)
 
-        app.use(cookieParser());
-        app.enableCors({ origin: "http://localhost:3000", credentials: true })
-        app.useGlobalPipes(new ValidationPipe())
+      app.use(cookieParser())
+      app.enableCors({origin: "*", credentials: true})
+      app.useGlobalPipes(new ValidationPipe())
 
-        await app.listen(PORT, () => console.log(`SERVER START ON PORT: ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
+      await app.listen(PORT, () => console.log(`SERVER START ON PORT: ${PORT}`))
+   } catch (e) {
+      console.log(e)
+   }
 }
 
 start()
