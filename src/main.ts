ba76ada9f8ee3e2c128de9ccdb9cefb788e5ebc7
Pavel <sh1k@bk.ru>
2021-07-19 22:02:21 +0300

    add Cors true

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ import {ValidationPipe} from './pipes/validation.pipe'
 const start = async () => {
    try {
       const PORT = process.env.PORT || 5000
-      const app = await NestFactory.create(AppModule)
+      const app = await NestFactory.create(AppModule, {cors: true})
 
       app.use(cookieParser())
-      app.enableCors({
-          origin: true,
-          methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-          credentials: true
+      /*app.enableCors({
+         origin: true,
+         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+         credentials: true
       })
-      app.useGlobalPipes(new ValidationPipe())
+      app.useGlobalPipes(new ValidationPipe())*/
 
       await app.listen(PORT, () => console.log(`SERVER START ON PORT: ${PORT}`))
    } catch (e) {
