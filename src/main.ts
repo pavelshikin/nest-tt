514f119f23a417a6a6d7fddf1ac83ddcffaab5c7
Pavel <sh1k@bk.ru>
2021-07-19 22:05:11 +0300

    add Cors options

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ import {ValidationPipe} from './pipes/validation.pipe'
 const start = async () => {
    try {
       const PORT = process.env.PORT || 5000
-      const app = await NestFactory.create(AppModule, {cors: true})
+      const app = await NestFactory.create(AppModule)
 
       app.use(cookieParser())
-      /*app.enableCors({
+      app.enableCors({
          origin: true,
-         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+         methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
          credentials: true
       })
-      app.useGlobalPipes(new ValidationPipe())*/
+      app.useGlobalPipes(new ValidationPipe())
 
       await app.listen(PORT, () => console.log(`SERVER START ON PORT: ${PORT}`))
    } catch (e) {
